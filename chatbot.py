efabc6bc4e51aa493a797b311c2fd0f96fdfab5d
Emma Takács <delczegemma@gmail.com>
2026-07-07 15:15:25 +0200

    feat: Add age guessing funcitonality

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,18 @@ def remind_name():
     # reading a name
     print(f"What a great name you have, {your_name}!")
 
+def age_guessing():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+    remainder3 = int(input())
+    remainder5 = int(input())
+    remainder7 = int(input())
+    # reading all remainders
+    your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+    print(f"Your age is {your_age}; that's a good time to start programming!")
 
 # Now we can use these functions
 greet("Aid", 2026)
 remind_name()
+age_guessing()
